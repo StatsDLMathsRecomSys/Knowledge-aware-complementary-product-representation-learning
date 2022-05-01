7af14aef67c9e13531156e2ed64b63ac54294c59
Chuanwei <ruanchuanwei@gmail.com>
2022-05-01 16:54:40 -0700

    Update dataLoader.cpp
    fix bug

--- src/dataLoader.cpp
+++ src/dataLoader.cpp
@@ -159,6 +159,7 @@ std::vector<std::vector<int32_t> > DataLoader::loadOrderedBasket(const std::stri
   const std::string commaDeli = ",";
 
   int64_t lineCount = 0;
+  int64_t errCount = 0;
   while (std::getline(ifs, line)) {
     lineCount += 1;
     std::vector<int32_t> currLine;
